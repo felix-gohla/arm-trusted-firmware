c37d243cfa846d296b0fa187e0ec7f7e4b8676fe
Felix Gohla <felix@felixgohla.de>
2025-03-21 08:55:13 +0100

    feat: add minimal version of SKTEE Dispatcher

---
+++ services/spd/skteed/skteed_smc.h
@@ -0,0 +1,13 @@
+/**
+ * Definitions for the SMC function IDs.
+ */
+
+#ifndef SKTEED_SMC_H
+#define SKTEED_SMC_H
+
+#define SKTEED_SMC_ENTRY_DONE		0xf2000000
+#define SKTEED_SMC_GET_UUID		0xf2000001
+#define SKTEED_SMC_PARAMTEST		0x72000003
+#define SKTEED_SMC_LONGRUNNING		0x72000004
+
+#endif /* SKTEED_SMC_H */
